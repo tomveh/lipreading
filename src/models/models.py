95945759f5a8ffc146c8621eb71e83c4264f5997
Tommi Vehviläinen <tommi.vehvilainen@protonmail.com>
2020-01-24 19:51:36 +0200

    Use only conv for pretraining

--- src/models/models.py
+++ src/models/models.py
@@ -1,20 +1,14 @@
 import torch.nn as nn
 
 from .frontend import VisualFrontEnd
-from .backends import ConvolutionalBackend, LSTMBackend
+from .backends import ConvolutionalBackend
 
 
-class LipreadingResNet(nn.Module):
-    def __init__(self, resnet, backend, nh=256):
+class PretrainNet(nn.Module):
+    def __init__(self, resnet, nh):
         super().__init__()
         self.frontend = VisualFrontEnd(out_channels=nh, resnet=resnet)
-
-        if backend == 'conv':
-            self.backend = ConvolutionalBackend(nh, 500)
-        elif backend == 'lstm':
-            self.backend = LSTMBackend(nh, 500)
-        else:
-            raise RuntimeError(f'Unknown backend type: {backend}')
+        self.backend = ConvolutionalBackend(nh, 500)
 
     def forward(self, x):
         x = self.frontend(x)
